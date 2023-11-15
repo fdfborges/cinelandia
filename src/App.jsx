7e47211efa13d8965efa1cce53e94c14b19090fc
Felipe Borges <felipedefreitasborges@gmail.com>
2023-11-14 22:44:46 -0300

    Created a linking of the API for comunication for photos of the movies

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,9 @@ function App() {
   return (
     <div className='App' >
       <Navbar />
-      <h2>Cinelandia</h2>
       <Outlet />
     </div>
   );
 }
 
-export default App
+export default App;
