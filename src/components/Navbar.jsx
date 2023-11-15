e0074fc1641d7856de0db7d28b39342404e34390
Felipe Borges <felipedefreitasborges@gmail.com>
2023-11-14 23:38:01 -0300

    Completede task of the button seach funtionalited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
 
 import "./Navbar.css";
+
 function Navbar() {
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(!search) return;
+
+        navigate(`/search?q=${search}`);
+        setSearch("");
+
+    }
+
     return (
         <nav id="navbar">
             <h2>
                 <Link to="/"><BiCameraMovie />Cinelândia</Link>
             </h2>
-            <form>
-                <input type="text" placeholder='Busque um Filme' />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Busque um Filme'
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
                 <button type='submit'>
                     <BiSearchAlt2 />
                 </button>
